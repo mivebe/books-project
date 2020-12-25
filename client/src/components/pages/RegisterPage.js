40331085b96dd53163c6fe73177b5e31424c46d0
mivebe <mivebe@abv.bg>
2020-12-25 11:11:15 +0200

    temp fix

--- client/src/components/pages/RegisterPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Register() {
-    return (
-        <div>
-            <p style={{ color: "white", fontSize: "50px", textAlign: "center" }}>You Have Successfully Registered</p>
-        </div>
-    )
-}
