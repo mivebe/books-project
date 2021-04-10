136bb4465fcb726b58128f984ad2628b3540e49e
mivebe <mivebe@abv.bg>
2021-04-10 17:09:35 +0300

    sass and 7-1 architecture layed down

--- client/src/components/switches/PrivateSwitch.js
+++ client/src/components/switches/PrivateSwitch.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Switch, Route, Redirect } from "react-router-dom"
-import Home from '../pages/HomePage'
+import HomePage from '../pages/HomePage'
 import PageNotFound from '../pages/PageNotFound';
 
 const PrivateSwitch = () => {
     console.log("Private Switch Entered");
     return (
         <Switch>
-            <Route exact path="/home"><Home /></Route>
+            <Route exact path="/home"><HomePage /></Route>
             <Route exact path="/404"><PageNotFound /></Route>
             <Redirect from="/logout" to="/login" />
             <Redirect from="*" to="/home" />
