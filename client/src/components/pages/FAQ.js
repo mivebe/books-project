3a1a6786ea392466e0dc501a223f81827637d816
mivebe <mivebe@abv.bg>
2021-09-28 10:32:26 +0300

    terms and faq pages created

---
+++ client/src/components/pages/FAQ.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FAQ = () => {
+    return (
+        <div>
+            THIS IS THE FAQ PAGE
+        </div>
+    )
+}
+
+export default FAQ
---
+++ client/src/components/pages/Terms.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Terms = () => {
+    return (
+        <div>
+            THIS IS THE TERMS PAGE
+        </div>
+    )
+}
+
+export default Terms
